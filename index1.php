137e49826d25e6149954413d1ca3746982655679
alexlapinskiy <olexsandr.lapinskiy@gmail.com>
2018-05-04 08:51:12 +0300

    Added to file variables_php array for images, delete unnecessary code from index1.php and main.css files

--- index1.php
+++ index1.php
@@ -20,20 +20,7 @@
 </head>
 
  <body>
-    <?php                                                                           // Add link`s to image as variables
-
-       /* $first_img="http://farm6.staticflickr.com/5614/15602332537_bae1aaccd8_m.jpg";
-        $second_img="http://farm8.staticflickr.com/7496/15959236842_6dbcb5b4c8_m.jpg";
-        $third_img="http://farm8.staticflickr.com/7404/12868632153_b0a5718c22_m.jpg";
-        $fourth_img="http://farm4.staticflickr.com/3864/14420515212_9999c800b4_m.jpg";
-        $fifth_img="http://farm8.staticflickr.com/7489/15241066224_8864806ffd_m.jpg";
-        $sixth_img="http://farm8.staticflickr.com/7412/8732133694_0c7e400022_m.jpg";
-        $seventh_img="http://farm4.staticflickr.com/3745/8971419780_cb88b22947_m.jpg";
-        $eighth_img="http://farm9.staticflickr.com/8366/8483546751_86494ae914_m.jpg";
-        $ninth_img="http://farm9.staticflickr.com/8481/8215602321_69d9939b8b_m.jpg";
-       */
-    ?>
-<div id="allimg">                                              <!-- Rewrite this part of code for link`s-variables -->
+  <div id="allimg">                                              <!-- Rewrite this part of code for link`s-variables -->
      <div id="img123">
          <img  src="<?php echo $first_img?>">
          <img id="img2" src="<?php echo $second_img?>">
